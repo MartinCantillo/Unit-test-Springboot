b0469e7c87478d2a2168e5d93dfacbd3529cb2c9
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-11-05 20:59:50 -0500

    adding Calculadora class

---
+++ Testing/src/main/java/com/example/demo/Calculadora.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo;
+
+/**
+ *
+ * @author marti
+ */
+public class Calculadora {
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        return a / b;
+    }
+}
