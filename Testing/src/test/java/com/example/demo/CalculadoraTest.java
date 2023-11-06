29fddacce30db4c8fdebed90137eb4aa02c24825
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-11-05 21:35:32 -0500

    Adding @beforeEach

--- Testing/src/test/java/com/example/demo/CalculadoraTest.java
+++ Testing/src/test/java/com/example/demo/CalculadoraTest.java
@@ -14,13 +14,19 @@ import org.junit.jupiter.api.*;
 
 public class CalculadoraTest {
     
+    Calculadora Calculadora;
+    
+    @BeforeEach
+    public void init(){
+        Calculadora =new Calculadora();
+        System.out.println("inicializando");
+    }
     @Test//para que lo reconozca como test Junit 
     public void testSuma(){
         //Valor esperado
         double esperado=25;
-        var Calculadora =new Calculadora();
         //valor obtenido
-        double obtenido=Calculadora.sumar(5.0, 10.0);
+        double obtenido=Calculadora.sumar(5.0, 20.0);
         //comparar esperado vs obtenido
         //assert de Junit para comparar
         Assertions.assertEquals(esperado, obtenido);
@@ -31,7 +37,6 @@ public class CalculadoraTest {
      @Test
     public void testResta(){
 
-        var Calculadora =new Calculadora();
         Assertions.assertEquals(50, Calculadora.restar(100.0, 50.0));
         //
         
@@ -40,7 +45,6 @@ public class CalculadoraTest {
      @Test
     public void testMultiplicacion(){
 
-        var Calculadora =new Calculadora();
         Assertions.assertEquals(10, Calculadora.multiplicar(2.0, 5.0));
         //
         
@@ -48,9 +52,11 @@ public class CalculadoraTest {
       @Test
     public void testDivicion(){
 
-        var Calculadora =new Calculadora();
         Assertions.assertEquals(2, Calculadora.dividir(4.0, 2.0));
         //
         
     }
+    
+    //inicializar el metodo calculadora en cada metodo es redundante para eso creo el objeto general y creo un metodo
+    //para que lo inicie antes de cada metodo para eso se le coloca la anotacion beforeeach
 }
